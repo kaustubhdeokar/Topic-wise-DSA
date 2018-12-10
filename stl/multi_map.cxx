6125004d721abece092a2b35c25dd934718215a9
kaustubhdeokar <kaustubhdcpo@gmail.com>
2018-12-10 21:28:37 +0530

    updated hashing

---
+++ stl/multi_map.cxx
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	//multimap<int,string>mp;
+	//int i,n,no;
+	cout<<cbrt(8)-round(8);
+/*	string name;
+	cin>>n;
+	for(i=0;i<n;i++)
+	{
+		cin>>no>>name;
+		mp.insert({no,name});
+	}
+	int q;
+	cin>>q;
+	multimap<int,int>::iterator it;
+	for(i=0;i<q;i++)
+	{
+		cin>>no;
+		auto it=mp.find(no);
+		cout<<it->second<<endl;
+	}
+	return 0;
+*/
+}
