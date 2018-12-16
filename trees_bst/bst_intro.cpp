098f4674c0298965279a41109d6c3a738f7c07de
kaustubhdeokar <kaustubhdcpo@gmail.com>
2018-12-16 19:29:31 +0530

    tree

--- trees_bst/bst_intro.cpp
+++ trees_bst/bst_intro.cpp
@@ -1,53 +1,52 @@
 #include<iostream>
 using namespace std;
 
-class TreeNode{
-	public:
-	int data;
-	TreeNode* left;
-	TreeNode* right;
-	TreeNode(int data){
-		this->data=data;
-		this->left=NULL;
-		this->right=NULL;
-	}
+class Node{
+
+    public:
+        int data;
+        Node* left;
+        Node* right;
+    Node(int data){
+        this->data=data;
+        this->left=NULL;
+        this->right=NULL;
+    }
 };
 
-TreeNode* root=NULL;
+Node* insert(Node* root,int data){
 
-void insert(int data){
-
-    TreeNode* newnode=new TreeNode(data);
-    cout<<root->data;
-    cout<<root->left;
-    cout<<root->right;
+    Node* newnode=new Node(data);
+    if(root==NULL)
+    root=newnode;
+    else if(data < (root->data))
+		root->left=insert(root->left,data);
+    else    
+        root->right=insert(root->right,data);
+    return root;
 }
 
-	void print(TreeNode* root){
-		
-		if(root==NULL)
-		return;
-		else
-		{
-			print(root->left);
-			cout<<root->data;
-			print(root->right);
-		}
+void print(Node* root){
+	if(root==NULL)
+	return;
+	else
+	{
+		print(root->left);
+		cout<<root->data;
+		print(root->right);
 	}
+}
 
 int main(){
-	
-	int n;
-	cin>>n;
-	int i;
-    TreeNode* head;
-	for(i=0;i<n;i++)
-	{
-		int temp;
-		cin>>temp;
-		head=insert(temp);
-	}
-	return 0;
+	Node* root=NULL;
+    int n,i;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        int temp;
+        cin>>temp;
+        root=insert(root,temp);
+		print(root);
+    }
+    return 0;
 }
-	
-	
