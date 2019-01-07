576a62184305d44a6900b418ebebc7632f0b1e49
kaustubhdeokar <kaustubhdcpo@gmail.com>
2019-01-07 18:13:45 +0530

    .

--- Hackerrank_interview/repeated_string.py
+++ Hackerrank_interview/repeated_string.py
@@ -7,12 +7,16 @@ import sys
 
 # Complete the repeatedString function below.
 def repeatedString(s, n):
+    toret=0
+    a_in_s=s.count('a')
     l=len(s)
-    m=math.ceil(n/l)
-    s=s*m
-    s=s[:10]
-    r=s.count('a')
-    return r
+    m=((a_in_s/l)*n)
+    toret+=m
+    n=n%l
+    s=s[:n]
+    a_in_s=s.count('a')
+    toret+=a_in_s
+    return toret
 if __name__ == '__main__':
     s = input()
     n = int(input())
