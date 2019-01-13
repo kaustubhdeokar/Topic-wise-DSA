e5f0cc6ac0e9727bbb0f54ac0709bc16a412960e
kaustubhdeokar <kaustubhdcpo@gmail.com>
2019-01-13 12:01:00 +0530

    graphdfs

--- graphs_/simple.cxx
+++ graphs_/simple.cxx
@@ -12,6 +12,9 @@ void dfsutil(int i,vector<int>adj[],vector<bool>&visited){
     for(int j=0;j<adj[i].size();j++){
         if(visited[adj[i][j]]==false)
             dfsutil(adj[i][j],adj,visited);
+        else{
+            cout<<"visited"<<adj[i][j]<<" "<<endl;
+        }
     }
 
 }
@@ -19,23 +22,26 @@ void dfsutil(int i,vector<int>adj[],vector<bool>&visited){
 void DFS(vector<int> adj[],int v){
     vector<bool> visited(v,false);
     int i;
-    for(i=0;i<1;i++){
+    for(i=0;i<v;i++){
         if(visited[i]==false)
             dfsutil(i,adj,visited);
-    }
+        else{
+            cout<<"already visited"<<i;
+        }
+    }   
 }
 int main(){
-    int V=6;
+    int V=5;
     vector<int>adj[V];
     addedge(adj, 0, 1); 
     addedge(adj, 0, 4); 
-    addedge(adj, 0, 5);
+
     addedge(adj, 1, 2); 
     addedge(adj, 1, 3); 
     addedge(adj, 1, 4); 
     addedge(adj, 2, 3); 
     addedge(adj, 3, 4);
-    addedge(adj, 4, 5); 
+     
     DFS(adj, V); 
     return 0;
 }
