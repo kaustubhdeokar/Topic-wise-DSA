59fcbdf5f5f5fa8041d3bbdf2c47ef71c8ed3073
kaustubhdeokar <kaustubhd9@gmail.com>
2019-06-06 23:16:57 +0530

    bits&trees:

--- DSA/trees_bst/bst_insertion_pre.cxx
+++ DSA/trees_bst/bst_insertion_pre.cxx
@@ -2,7 +2,7 @@
  * this program consists of insertion in a  binary tree
  * height of a binary tree
 */
-
+#include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
 
@@ -40,29 +40,29 @@ void print(Node* root){
 	return;
 	else
 	{
-		cout<<root->data<<endl;
 		print(root->left);
+		cout<<root->data<<" ";
 		print(root->right);
 	}
 }
 
-void print(Node* root,int temp){
-	
-	if(root==NULL)
-	return;
-	else
+int checkbalanced(Node* head){
+
+	if(head==NULL)
+	return 0;
+	else{
+		int l=checkbalanced(head->left);
+		int r=checkbalanced(head->right);
+		if(abs(l-r)>1)
 		{
-			if(root->data==temp){
-			cout<<root->data<<endl;
-			print(root->left);
-			print(root->right);
-			}
-			else
-			{
-			print(root->left,temp);
-			print(root->right,temp);
-			}
+			return -1;
 		}
+		if(l>r)
+		return l+1;
+		else
+		return r+1;
+	}
+	return 1;
 }
 
 
@@ -75,8 +75,11 @@ int main(){
 		cin>>temp;
 		head=placeNode(head,temp);
 	}
-	cin>>temp;
-	print(head,temp);
-
+	int a=checkbalanced(head);
+	cout<<a;
+	if(a!=-1)
+	cout<<"true";
+	else
+	cout<<"false";
 	return 0;
 }
