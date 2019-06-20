2ef247e63ef3f20fd306d36c225b21559f82ba85
kaustubhdeokar <kaustubhd9@gmail.com>
2019-06-20 19:35:16 +0530

    heaps_median_stream:

--- Algos/graphs_/bfs_dfs.cpp
+++ Algos/graphs_/bfs_dfs.cpp
@@ -1,71 +1,41 @@
-//permutations pallindrome.
 #include<iostream>
-#include<bits/stdc++.h>
-#include<algorithm>
-#include<cmath>
 using namespace std;
 
-void print(vector<int>v[],int nodes){
-    for(int i=1;i<=nodes;i++){
-        cout<<i<<" ";
-        for(int j=0;j<v[i].size();j++){
-            cout<<v[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-
+int parent(int i){return (i-1)/2;}
+int left(int i){return (2*i)+1;}
+int right(int i){return (2*i)+2;}
+void swap(int *x,int *y){
+	int temp=*x;
+	*x=*y;
+	*y=temp;
 }
-
-void bfs(vector<int>v[],int nodes,int head){
+void heapify(int arr[],int n){
     
-    queue<int>q;
-    q.push(head);
-    bool vis[nodes+1]={0};
-    vis[head]=true;
-    while(!q.empty()){
-        int p=q.front();
-        q.pop();
-        cout<<p<<" ";
-        for(int i=1;i<=nodes;i++){
-            for(int j=0;j<v[i].size();j++){
-                if(vis[v[i][j]]!=1)
-                {
-                    q.push(v[i][j]);
-                    vis[v[i][j]]=1;
-                }
-            }
-        }
-    }
-
+    for(int i=0;i<n;i++){
+		int j=i;
+		while(j!=0)
+		{
+			if(arr[parent(j)]>arr[j])
+			swap(&arr[parent(j)],&arr[j]);
+			j=parent(j);
+		}
+	}
+    
+    for(int i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+    
 }
 
-void dfs(vector<int>v[],int head,bool visited[]){
 
-    visited[head]=1;
-    cout<<head<<" ";
-    for(int i=0;i<v[head].size();i++){
-        if(visited[v[head][i]]!=1)
-        {
-            dfs(v,v[head][i],visited);
-        }
-    }
 
-}
 
 int main(){
-    int n,nodes,edges;
-    cin>>nodes>>edges;
-    vector<int>v[10];
-    int x,y;
-    for(int i=1;i<=edges;i++){
-        cin>>x>>y;
-        v[x].push_back(y);
-    }
-//    print(v,nodes);
-    bfs(v,nodes,1);
-    cout<<endl;
-    bool visited[nodes+1]={0};  
-    dfs(v,1,visited);
 
+    int *arr,n;
+    cin>>n;
+    arr=new int[n];
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+    heapify(arr,n);
     return 0;
 }
--- Algos/graphs_/topological_sort.cpp
+++ Algos/graphs_/topological_sort.cpp
@@ -1,37 +1,9 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-int digitSum(int n) { 
-	int ans = 0; 
-	while (n) { 
-		ans=ans+(n % 10); 
-		n=n/10; 
-	} 
-	return ans; 
-} 
+#include <bits/stdc++.h>
+#include<iostream> 
+using namespace std;
 
-int findInt(int n, int m) { 
-	int minDigit = floor(m/ 9); 
-	int start = pow(10, minDigit) - 
-				(int)pow(10, minDigit) % n; 
-	while (start < INT_MAX) { 
-		if (digitSum(start) == m) 
-			return start; 
-		else
-			start += n; 
-	} 
-	return -1; 
-} 
-
-int main() 
-{ 
-	int t;
-	cin>>t;
-	while(t){
-	int n;
-	cin>>n;
-	int m = 10; 
-	cout << findInt(n, m); 
-	t--;
-	}
-	return 0; 
-} 
+void foo(int *arr[]){
+	char *st="hello";
+	cout<<st;
+	return 0;
+}
