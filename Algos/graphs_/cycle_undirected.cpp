86ca051a06614a814bada15d4e2e81627a0a1c76
kaustubhdeokar <kaustubhd9@gmail.com>
2019-07-03 23:35:46 +0530

    .

---
+++ Algos/graphs_/cycle_undirected.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+int temp=1;
+bool isCyclic(vector<int>graph[],int nodes,int head,bool visited[],int parent[]){
+		
+		visited[head]=true;
+		int l=graph[head].size();
+		for(int i=0;i<l;i++)
+		{
+			if(parent[graph[head][i]]==-1)
+			parent[graph[head][i]]=head;
+			else if(parent[graph[head][i]]!=head)
+			{	
+				temp=0;
+				return true;
+			}
+			isCyclic(graph,nodes,graph[head][i],visited,parent);
+		}
+		if(temp!=0)
+		return false;
+		else
+		return true;
+}
+
+/*
+1
+4 4
+0 1 1 2 1 3 2 3
+*/
+
+int main() 
+{ 
+	int nodes,edges,t;
+	cin>>t;
+	while(t--){
+		cin>>nodes>>edges;
+		int u,v;
+		vector<int>graph[1000];
+		for(int i=0;i<edges;i++){
+			cin>>u>>v;
+			graph[u].push_back(v);
+		}
+		bool visited[nodes];
+		memset(visited,false
+,sizeof(bool)*nodes);
+		int parent[nodes];
+		memset(parent,-1,sizeof(int)*nodes);
+		cout<<isCyclic(graph,nodes,0,visited,parent);
+	}
+	return 0; 
+} 
+
--- Algos/graphs_/topological_sort.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-#include<iostream> 
-using namespace std;
-
-void foo(int *arr[]){
-	char *st="hello";
-	cout<<st;
-	return 0;
-}
