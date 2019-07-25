fce24665defa0803139eaf38a27d4cb8e2698616
kaustubhdeokar <kaustubhd9@gmail.com>
2019-07-25 15:21:34 +0530

    trees

---
+++ DSA/trees/vector_inside_vector.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    vector<vector<int>>v;
+
+    vector<int>b;
+    b.push_back(1);
+    b.push_back(1);
+    b.push_back(1);
+    v.push_back(b);
+    return 0;
+
+}
