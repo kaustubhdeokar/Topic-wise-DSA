1f02b9dbb929044a1341796d20cbc6fb24f1a7c4
kaustubhdeokar <kaustubhd9@gmail.com>
2022-04-13 08:15:51 +0530

    log-method-to-count-digits

---
+++ 2/array/problems/NoOfDigits.java
@@ -0,0 +1,20 @@
+package linearsearch;
+
+public class NoOfDigits {
+
+    public static void main(String[] args) {
+
+        int[] arr = {12, 345, 2, 6, 7896};
+        int count = 0;
+        for (int j : arr) {
+            int noOfDigits = calculateDigitsIn(j);
+            if ((noOfDigits & 1) == 0) count += 1;
+        }
+        System.out.println(count);
+    }
+
+    public static int calculateDigitsIn(int num) {
+        if (num < 1) num *= -1;
+        return (int) (Math.log10(num)) + 1;
+    }
+}
