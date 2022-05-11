6b860bc97f516bb1165ddec54609f8a3adb6a400
kaustubhdeokar <kaustubhd9@gmail.com>
2022-05-11 09:14:34 +0530

    tree easy leetcode

---
+++ 2/trees/invertTree.java
@@ -0,0 +1,15 @@
+ public TreeNode invertTree(TreeNode root) {
+        if(root == null)
+            return null;
+        if(root.left == null && root.right ==null)
+            return root;
+        
+        invertTree(root.left);
+        invertTree(root.right);
+        
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        return root;
+    }
