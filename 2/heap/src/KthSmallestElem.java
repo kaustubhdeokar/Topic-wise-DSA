7b28b2ff994956f7a939f4f094590748e59b8390
kaustubhdeokar <kaustubhd9@gmail.com>
2022-06-16 23:39:32 +0530

    heap kth smallest.

---
+++ 2/heap/src/KthSmallestElem.java
@@ -0,0 +1,20 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class KthSmallestElem {
+
+    public static void main(String[] args) {
+
+        Comparator<Integer> comparator = (i1,i2) -> Integer.compare(i2,i1);
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(comparator);
+        int k =3;
+        int arr[] = {7, 10, 4, 3, 20, 15};
+        for (int i : arr) {
+            priorityQueue.add(i);
+            if(priorityQueue.size()>k)
+                priorityQueue.poll();
+        }
+        System.out.println(priorityQueue.poll());
+    }
+
+}
