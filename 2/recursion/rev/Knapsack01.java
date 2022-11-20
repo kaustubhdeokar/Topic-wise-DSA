1cdf88c939c9b344292ba93d58e1da2bf1b1982a
kaustubhdeokar <kaustubhd9@gmail.com>
2022-11-20 12:12:35 +0530

    partition equal subset recursive

--- 2/recursion/rev/Knapsack01.java
+++ 2/recursion/rev/Knapsack01.java
@@ -18,8 +18,9 @@ public class Knapsack01 {
             return 0;
             
         if(wt[n-1]<=W){
-            return Integer.max(val[n-1]+knapSack(W-wt[n-1], wt, val, n-1),
-                                knapSack(W, wt, val, n-1));
+            return Integer.max(
+            val[n-1]+knapSack(W-wt[n-1], wt, val, n-1),
+            knapSack(W, wt, val, n-1));
         }
         else{
             return knapSack(W, wt, val, n-1);
