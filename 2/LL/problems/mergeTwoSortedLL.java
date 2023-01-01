3684f85d8d8289a11aa72d9b54ca7c87f5f5896c
kaustubhdeokar <kaustubhd9@gmail.com>
2023-01-01 21:49:16 +0530

    leetcode 75

--- 2/LL/problems/mergeTwoSortedLL.java
+++ 2/LL/problems/mergeTwoSortedLL.java
@@ -41,5 +41,27 @@ class mergeTwoSortedLL {
 
     }
 
+    public Node mergeWithNewList(Node head1, Node head2) {
+
+        Node newHead = new Node(0);
+        Node tail = newHead;
+
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                tail.next = head1;
+                head1 = head1.next;
+            } else {
+                tail.next = head2;
+                head2 = head2.next;
+            }
+            tail = tail.next;
+        }
+        if (head1 == null)
+            tail.next = head2;
+        else
+            tail.next = head1;
+
+        return newHead.next;
+    }
 
 }
