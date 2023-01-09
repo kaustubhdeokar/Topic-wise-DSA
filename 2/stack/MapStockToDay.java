51e696242501c4a9c377a98466c233003e5a9491
kaustubhdeokar <kaustubhd9@gmail.com>
2023-01-10 00:13:20 +0530

    stack questions

---
+++ 2/stack/MapStockToDay.java
@@ -0,0 +1,9 @@
+public class MapStockToDay {
+    int stockPrice;
+    int day;
+    MapStockToDay(int stockPrice, int day){
+        this.stockPrice = stockPrice;
+        this.day = day;
+    }
+
+}
