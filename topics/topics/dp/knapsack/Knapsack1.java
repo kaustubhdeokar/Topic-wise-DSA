8e6d585ab9c06a5478086fba2cedd6cba2e3996e
kaustubhdeokar <kaustubhd9@gmail.com>
2024-12-29 21:05:43 +0530

    dp revise

--- topics/topics/dp/knapsack/Knapsack1.java
+++ topics/topics/dp/knapsack/Knapsack1.java
@@ -11,21 +11,51 @@ public class Knapsack1 {
 
         int N = 3;
         int W = 4;
+
         Knapsack1 knapsack = new Knapsack1();
-        System.out.println(knapsack.findOptimalWeight(values, weight, N - 1, W));
 
-        int[][] dp = new int[N+1][W + 1];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
+        System.out.println(knapsack.withRecursion(N, W, values, weight));
 
-        System.out.println(knapsack.findOptimalWeight(dp, values, weight, N, W));
+        System.out.println(knapsack.withMemoization(N, W, values, weight));
 
-        System.out.println(knapsack.findOptimalWeightBottomUp(values, weight, N, W));
+        System.out.println(knapsack.withTabulation(N, W, values, weight));
     }
 
-    private int findOptimalWeightBottomUp(int[] values, int[] weight, int N, int W) {
+    public int withRecursion(int N, int W, int[] values, int[] weight) {
+        if (N <= 0) return 0;
+        if (W >= weight[N - 1]) {
+            return Integer.max(
+                    withRecursion(N - 1, W - weight[N - 1], values, weight) + values[N - 1],
+                    withRecursion(N - 1, W, values, weight));
+        } else {
+            return withRecursion(N - 1, W, values, weight);
+        }
+    }
+
+    private int withMemoization(int N, int W, int[] values, int[] weight) {
+        int[][] memo = new int[N + 1][W + 1];
+        for (int[] m : memo) Arrays.fill(m, -1);
+        return withMemoization(memo, N, W, values, weight);
+    }
+
+    public int withMemoization(int[][] memo, int N, int W, int[] values, int[] weight) {
+        if (N <= 0) return 0;
+        if (memo[N][W] != -1) return memo[N][W];
+
+        if (W >= weight[N - 1]) {
+            memo[N][W] = Integer.max(
+                    withMemoization(memo, N - 1, W - weight[N - 1], values, weight) + values[N - 1],
+                    withMemoization(memo, N - 1, W, values, weight));
+        } else {
+            memo[N][W] = withMemoization(memo, N - 1, W, values, weight);
+        }
+        return memo[N][W];
+    }
+
+    private int withTabulation(int N, int W, int[] values, int[] weight) {
+
         int[][] dp = new int[N + 1][W + 1];
+
         for (int i = 0; i <= N; i++) {
             dp[i][0] = 0;
         }
@@ -33,32 +63,17 @@ public class Knapsack1 {
             dp[0][i] = 0;
         }
 
-        for (int n = 1; n <= N; n++) {
-            for (int w = 1; w <= W; w++) {
-                if (weight[n-1] <= w) {
-                    dp[n][w] = Integer.max(values[n-1] + dp[n - 1][w - weight[n-1]], dp[n - 1][w]);
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= W; j++) {
+                if (j >= weight[i - 1]) {
+                    dp[i][j] = Integer.max(dp[i - 1][j - weight[i - 1]] + values[i - 1], dp[i - 1][j]);
                 } else {
-                    dp[n][w] = dp[n - 1][w];
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
-        printArr(N, W, dp);
+//        printArr(N, W, dp);
         return dp[N][W];
-    }
-    //top-down approach.
-    private int findOptimalWeight(int[][] dp, int[] values, int[] weight, int n, int w) {
-
-        if (n <= 0 || w <= 0) return 0;
-
-        if (dp[n][w] != -1) return dp[n][w];
-
-        if (w >= weight[n-1]) {
-            dp[n][w] = Integer.max(values[n-1] + findOptimalWeight(dp, values, weight, n - 1, w - weight[n-1]),
-                    findOptimalWeight(dp, values, weight, n - 1, w));
-        } else {
-            dp[n][w] = findOptimalWeight(dp, values, weight, n - 1, w);
-        }
-        return dp[n][w];
     }
 
     private static void printArr(int N, int W, int[][] dp) {
@@ -67,24 +82,6 @@ public class Knapsack1 {
                 System.out.print(dp[i][j] + " ");
             }
             System.out.println();
-        }
-    }
-
-    //recursive approach.
-    public int findOptimalWeight(int[] values, int[] weight, int N, int W) {
-
-        if (N < 0 || W == 0) {
-            return 0;
-        }
-
-        if (weight[N] <= W) {
-
-            int a = values[N] + findOptimalWeight(values, weight, N - 1, W - weight[N]);
-            int b = findOptimalWeight(values, weight, N - 1, W);
-            return Integer.max(a, b);
-        } else {
-            int c = findOptimalWeight(values, weight, N - 1, W);
-            return c;
         }
     }
 
