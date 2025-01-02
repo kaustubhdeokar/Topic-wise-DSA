e783c044c83ff57e454b9c1b1fa13a07251c825c
kaustubhdeokar <kaustubhd9@gmail.com>
2025-01-02 23:11:57 +0530

    lc daily

---
+++ topics/topics/prefixsum/problems/CountVowelInRange.java
@@ -0,0 +1,34 @@
+package prefixsum.problems;
+
+import java.util.Set;
+
+public class CountVowelInRange {
+    public int[] vowelStrings(String[] words, int[][] queries) {
+
+        int[] result = new int[queries.length];
+        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
+        int count = 0;
+        int[] dp = new int[words.length];
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (vowels.contains(word.charAt(0)) && vowels.contains(word.charAt(word.length() - 1))) {
+                count += 1;
+            }
+            dp[i] = count;
+        }
+
+        for (int i = 0; i < queries.length; i++) {
+            int start = queries[i][0];
+            if (start != 0) {
+                result[i] = dp[queries[i][1]] - dp[queries[i][0] - 1];
+            } else {
+                result[i] = dp[queries[i][1]];
+            }
+        }
+
+        return result;
+
+    }
+    
+}
